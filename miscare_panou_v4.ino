b7c03cc1ea8aaa76eb2a6dd9f2cf152ab32885e1
Vlad Dumitru <76907292+vladgabriel24@users.noreply.github.com>
2023-05-12 12:00:04 +0300

    Create miscare_panou_v4.ino
    Aceasta versiune este pentru cazul in care fotorezistoarele sunt lipite de panoul solar.
    Am adaugat functionalitatea de a reveni panoul in pozitia neutra cand este intuneric.

---
+++ miscare_panou_v4.ino
@@ -0,0 +1,193 @@
+#include <LiquidCrystal.h>
+
+#define MAX_VAL 20 // Valoarea maxima a contorului ce corespunde pozitiei de +180 grade a panoului solar
+#define PRAG 1.20f // Valoarea in modul a pragului impus pentru miscarea panoului
+#define TIMESTAMP 20 // Cuanta de timp in milisecunde
+#define SPEED 170 // Viteza motorului
+
+int sensorPin0 = A0; // Pinul ce va primi tensiunea de la fotodioda din stanga -> pinul A0
+int sensorPin1 = A1; // Pinul ce va primi tensiunea de la fotodioda din dreapta -> pinul A1
+
+int sensorValue0; // valoarea citita de pinul A0 <=> valoarea tensiunii prin fotorezistorul din stanga
+int sensorValue1; // valoarea citita de pinul A1 <=> valoarea tensiunii prin fotorezistorul din dreapta
+
+float voltage0; // valoarea tensiunii prin fotorezistorul din stanga, convertita in volti
+float voltage1; // valoarea tensiunii prin fotorezistorul din dreapta, convertita in volti
+
+float diff; // diferenta dintre tensiunea data de fotorez din dreapta si cea data de fotorez din stanga
+
+int contor = MAX_VAL/2; // contorul ce va exprima pozitia de la -90 la +90 grade
+                        // valoarea 0 va corespunde pozitiei de -90 grade
+                        // valoarea MAX_VAL va corespunde pozitiei de +90 grade
+                        // valoarea MAX_VAL/2 va corespunde pozitiei de 0 grade (pozitia neutra)
+
+int prag_intuneric = 4.80f; // Valoarea tensiunii de pe fotorezistor ce semnifica faptul ca se afla intr-un loc intunecat
+
+// Pinii de control pentru L298N
+int enA = 9; // pinul 9
+int in1 = 8; // pinul 8
+int in2 = 7; // pinul 7
+
+int unit_grade = 180/MAX_VAL;
+int grade = 0;
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+// Legaturile sunt efectuate in modul urmator:
+// VSS la masa si VDD la 5V
+
+// RS la pinul 12 digital
+// RW la masa
+// EN la pinul 11 digital
+
+// D4 la pinul 5 digital
+// D5 la pinul 4 digital
+// D6 la pinul 3 digital
+// D7 la pinul 2 digital
+
+void setup() {
+
+  Serial.begin(9600); // initializam seriala cu baudrate-ul specific
+  lcd.begin(16,2);
+
+  // Setam toti pinii de control al L298N ca mod de operare = OUTPUT
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+}
+
+void miscare_panou_stanga()
+{
+  // Miscam panoul catre stanga:
+  // Prin setarea starii ambilor pini la LOW, motorul se va opri
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA, SPEED); // Viteza motorului
+}
+
+void miscare_panou_dreapta()
+{
+  // Miscam panoul catre dreapta:
+  // Prin setarea starii pinului 8 la HIGH si pinului 7 la LOW, motorul se va deplasa la dreapta
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, SPEED); // Viteza motorului
+}
+
+void oprire_panou()
+{
+  // Panoul se va opri:
+  // Prin setarea starii pinului 8 la LOW si pinului 7 la HIGH, motorul se va deplasa la stanga
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 0); // Setez viteza motorului la 0
+}
+
+void print_unghi_pozitie()
+{
+  Serial.print("Unghi: ");
+  Serial.print(grade);
+  Serial.println(" grade");
+}
+
+void print_pozitie_lcd(int grade)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pozitie:");
+  lcd.setCursor(0, 1);
+  lcd.print(grade);
+  lcd.print(" grade");
+}
+
+void loop() {
+  
+  sensorValue0 = analogRead(sensorPin0); // citim tensiunea de pe fotorez din stanga
+  sensorValue1 = analogRead(sensorPin1); // citim tensiunea de pe fotorez din dreapta
+
+  voltage0 = sensorValue0 * (5.0/1023.0); // convertim valoarea citita (tens pe fotorez din stanga), in volti
+  voltage1 = sensorValue1 * (5.0/1023.0); // convertim valoarea citita (tens pe fotorez din dreapta), in volti
+  
+  // Afisam tensiunile celor doua fotorez
+  Serial.print("A0: ");
+  Serial.print(voltage0);
+  Serial.println("V");
+
+  Serial.print("A1: ");
+  Serial.print(voltage1);
+  Serial.println("V");
+
+  // Calculam diferenta intre tensiunea care cade pe fotorez din stanga si cea care cade pe fotorez din dreapta
+  diff = voltage0 - voltage1;
+
+  // Daca diferenta trece de un anumit prag pozitiv
+  if(diff >= PRAG)
+  {
+    if(contor < MAX_VAL) // Daca pozitia panoului nu a ajuns la +90 grade
+    {
+      Serial.println("DREAPTA!"); // Afisam string-ul "DREAPTA!" pe seriala
+      miscare_panou_dreapta();
+        
+      contor++; // Crestem contorul (crestem gradele catre +90)
+      grade = grade + unit_grade;
+    }
+    else // Daca pozitia panoului a ajuns la +90 grade
+    {
+      Serial.println("STOP!"); // Afisam string-ul "STOP!" pe seriala
+      oprire_panou();
+    }
+  }
+  else if(diff <= -PRAG) // Daca diferenta trece de un anumit prag negativ
+  {
+    if(contor > 0) // Daca pozitia panoului nu a ajuns la -90 grade
+    {
+      Serial.println("STANGA!"); // Afisam string-ul "STANGA!" pe seriala
+      miscare_panou_stanga();
+
+      contor--; // Scadem contorul (scadem gradele catre -90)
+      grade = grade - unit_grade;
+    }
+    else // Daca pozitia panoului a ajuns la -90 grade
+    {
+      Serial.println("STOP!"); // Afisam string-ul "STOP!" pe seriala
+      oprire_panou();
+    }
+  }
+  else // Daca diferenta este sub un anumit prag atat pe partea pozitiva cat si pe partea neg (in modul este mai mica decat pragul stabilit)
+  {
+    // Daca cele doua tensiuni date de fotorezistoare au atins pragul de intuneric si nu se afla la pozitia neutra de 0 grade
+    if(voltage0 >= prag_intuneric && voltage1 >= prag_intuneric && contor != MAX_VAL/2)
+    {
+      // Aduc panoul la 0 grade
+      if(contor > MAX_VAL/2)
+      {
+        while(contor != MAX_VAL/2)
+        {
+          miscare_panou_stanga();
+          contor--; // Scadem contorul (scadem gradele catre -90)
+          grade = grade - unit_grade;
+        }
+      }
+      else if(contor < MAX_VAL/2)
+      {
+         while(contor != MAX_VAL/2)
+        {
+          miscare_panou_dreapta();
+          contor++; // Scadem contorul (scadem gradele catre -90)
+          grade = grade + unit_grade;
+        }
+      }
+    }
+    else 
+    {
+      Serial.println("STOP!"); // Afisam string-ul "STOP!" pe seriala
+      oprire_panou();
+    }
+  }
+
+  print_unghi_pozitie();
+
+  print_pozitie_lcd(grade);
+
+  delay(TIMESTAMP);
+}
